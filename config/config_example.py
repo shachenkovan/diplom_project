6430b587160f3e435787c0e580cb6429edd14c0b
Anastasia Shachenkova <nastya.nba888@gmail.com>
2025-09-23 14:39:23 +0300

    fix config

--- config/config_example.py
+++ config/config_example.py
@@ -1,12 +1,19 @@
 """
 Конфигурация Flask приложения.
 Этот файл содержит настройки базы данных, безопасности и загрузки файлов.
-Для запуска замените значения на реальные.
+Для запуска создайте свой .env файл с указанием значений переменных согласно иструкции.
 """
+import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@db:3306/db_name' # укажите вашу бд
+MYSQL_USER = "root"
+MYSQL_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DATABASE}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'your-secret-key' # укажите свой секретный ключ
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "static/uploads")
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'txt'}
-UPLOAD_FOLDER= 'static/uploads'
